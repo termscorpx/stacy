7311f557a89586ab81607ac2254c80a9ac381d55
Anton Mur <mibus32@gmail.com>
2016-02-15 17:32:23 +0300

    parse fragment

--- fomatter.rb
+++ fomatter.rb
@@ -2,9 +2,13 @@ require 'nokogiri'
 
 
 Dir.glob('*.html') do |filename|
-  doc = Nokogiri::HTML(open(filename))
+  File.open(filename, 'w') do |file|
+    doc = Nokogiri::HTML.parse(file)
+    
+    doc.css('a').each do |link_node|
+      link_node['href'] = link_node['href'].gsub('https://termsdev.co', '.')
+    end
 
-  doc.css('a').each do |link_node|
-    link_node['href'] = link_node['href'].gsub('https://termsdev.co', '.')
+    file.write doc.to_html
   end
 end
