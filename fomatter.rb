922d9840eacc1438c459a49845870a8946ecff01
Anton Mur <mibus32@gmail.com>
2016-02-15 18:28:55 +0300

    fix links

--- fomatter.rb
+++ fomatter.rb
@@ -8,12 +8,16 @@ Dir.glob('*.html') do |filename|
   doc.css('a').each do |link_node|
     if link_node['href'].start_with?('https://termsdev.co/stacy')
       link_node['href'] = link_node['href'].gsub 'https://termsdev.co/stacy', '.'
-      link_node['href'] +='.html' if link_node['href'] =~ /https:\/\/termsdev.co\/stacy\/.*/
+      link_node['href'] += '.html' if link_node['href'] =~ /https:\/\/termsdev.co\/stacy\/.*/
     end
 
     if link_node['href'] == '.'
       link_node['href'] = 'stacy.html'
     end
+  end
+
+  doc.css('.lookbook-nav__link').each do |nav_node|
+    nav_node['href'] += '.html' unless nav_node['href'].end_with?('.html')
   end
 
   File.write(filename, doc.to_html)
